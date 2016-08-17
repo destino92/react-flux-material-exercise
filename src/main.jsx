20fb8bd9e9d36076e57333f075e69dafb3d764fe
destino92 <destinodello@gmail.com>
2016-08-17 20:43:34 +0300

    chore: added react material dependenci for tap and click event

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var injectTapEventPlugin = require('react-tap-event-plugin/src/injectTapEventPlugin.js');
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
