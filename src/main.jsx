093e7f8f128a30a1d488256093f79a0336ce1441
destino92 <destinodello@gmail.com>
2016-08-17 20:48:16 +0300

    style: require material ui theme for the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var injectTapEventPlugin = require('react-tap-event-plugin/src/injectTapEventPlugin.js');
+var MuiThemeProvider = require('material-ui/styles/MuiThemeProvider');
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
